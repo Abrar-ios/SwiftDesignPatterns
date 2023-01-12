d02bffaa2111c25baf9cf669dc0a5559c5bdc4a0
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-13 02:59:04 +0500

    One example of using singleton
    * how singleton can use to read required value/state in application as done in app delegate

--- SwiftDesignPatterns/AppDelegate.swift
+++ SwiftDesignPatterns/AppDelegate.swift
@@ -13,6 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if let isDeveloperModeEnabled = AppSettings.shared.int(for: "DeveloperModeEnabled") {
+            print("DeveloperModeEnabled:\(isDeveloperModeEnabled)")
+        }
         // Override point for customization after application launch.
         return true
     }
