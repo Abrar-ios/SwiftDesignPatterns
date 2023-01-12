a4f04e834d2fee8f2419b11718814c58641db5a9
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-13 01:54:23 +0500

    Add a dictionary to save/ read app settings

--- SwiftDesignPatterns/AppSettings.swift
+++ SwiftDesignPatterns/AppSettings.swift
@@ -10,6 +10,8 @@ import Foundation
 final public class AppSettings {
     public static let shared = AppSettings()
     
+    var settings : [String:Any] = ["DeveloperModeEnabled": 1, "AllowToUpload": 0, "GreetingLogo":"welcome"]
+    
     private init(){}
     
 }
