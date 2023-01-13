e19b4e62b1263bab345a5728e8a0ec083df2b1e4
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-13 17:20:05 +0500

    Implement cloning reference type objects
    * clone reference type objects
    * updating one object will change/impact other one object

--- SwiftDesignPatterns/Prototype/CloningReferenceType.playground/Contents.swift
+++ SwiftDesignPatterns/Prototype/CloningReferenceType.playground/Contents.swift
@@ -1,1 +1,28 @@
+class NameClass {
+    
+    var firstName: String
+    var lastName: String
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+}
 
+extension NameClass: CustomStringConvertible {
+    public var description : String {
+        return "NameClass: FirstName:\"\(firstName)\" LastName:\"\(lastName)\""
+    }
+}
+
+var usman = NameClass(firstName: "Usman", lastName: "Salman")
+var jawad = usman
+print(usman)
+print(jawad)
+
+jawad.firstName = "Jawad"
+jawad.lastName = "Naeem"
+
+print(usman)
+print(jawad)
