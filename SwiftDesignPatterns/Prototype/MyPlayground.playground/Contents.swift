34747903072bb6c45c374d8a256679ac1997c96d
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-13 16:50:24 +0500

    Implement Prototype for value type instances
    * Value type instances are ideal candidates for prototype.
    * There is no need of new line of code (implementation) to implement PROTOTYPE design pattern when need to create multiple instances of value type

--- SwiftDesignPatterns/Prototype/MyPlayground.playground/Contents.swift
+++ SwiftDesignPatterns/Prototype/MyPlayground.playground/Contents.swift
@@ -1,3 +1,16 @@
-import UIKit
+struct nameSruct {
+    var firstName: String
+    var lastName: String
+}
 
-var greeting = "Hello, playground"
+var haider = nameSruct(firstName: "Haider Ali", lastName: "Rizvi")
+var faheem = haider
+
+print("Haider:\(haider)")
+print("Faheem:\(faheem)")
+
+faheem.firstName = "Faheem"
+faheem.lastName = "Imran"
+
+print("Haider:\(haider)")
+print("Faheem:\(faheem)")
