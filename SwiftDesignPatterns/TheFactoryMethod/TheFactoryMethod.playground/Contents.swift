bf612f4cb800065629197ce388ee699d003cbe04
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-14 18:24:13 +0500

    Implementation of Factory Method
    * Using protocol with multiple types sharing same responsibilities
    * Implemented serializer example using global function as caller

--- SwiftDesignPatterns/TheFactoryMethod/TheFactoryMethod.playground/Contents.swift
+++ SwiftDesignPatterns/TheFactoryMethod/TheFactoryMethod.playground/Contents.swift
@@ -1,1 +1,44 @@
+protocol Serializable {
+    func serialize()
+}
+
+class JSONSerialIzer : Serializable {
+    func serialize() {
+        print("JSONSerialIzer:\(#function)")
+    }
+}
+
+class PlistSerialIzer : Serializable {
+    func serialize() {
+        print("PlistSerialIzer:\(#function)")
+    }
+}
+
+class XMLSerialIzer : Serializable {
+    func serialize() {
+        print("XMLSerialIzer:\(#function)")
+    }
+}
+
+enum Serializers {
+    case json
+    case plist
+    case xml
+}
+
+func makeSerializer(_ type: Serializers) -> Serializable? {
+    var result : Serializable?
+    switch type {
+    case .json: result = JSONSerialIzer()
+    case .plist: result = PlistSerialIzer()
+    case .xml: result = XMLSerialIzer()
+    }
+    return result
+}
+
+let jsonSerializer = makeSerializer(.json)
+jsonSerializer?.serialize()
+
+let plistSerializer = makeSerializer(.plist)
+plistSerializer?.serialize()
 
