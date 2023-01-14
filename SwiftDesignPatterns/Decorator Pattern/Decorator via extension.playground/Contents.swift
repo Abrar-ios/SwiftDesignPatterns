2333e150fb9a825c465b8674970b71f0a74e4e01
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-14 21:05:02 +0500

    Implement decorator using swift extension

--- SwiftDesignPatterns/Decorator Pattern/Decorator via extension.playground/Contents.swift
+++ SwiftDesignPatterns/Decorator Pattern/Decorator via extension.playground/Contents.swift
@@ -1,1 +1,15 @@
+import Foundation
 
+extension UserDefaults {
+    func set(date: Date?, forKey key: String){
+        self.set(date, forKey: key)
+    }
+    
+    func date(forKey key: String) -> Date? {
+        return self.value(forKey: key) as? Date
+    }
+}
+
+let userDefaults = UserDefaults.standard
+userDefaults.set(date: Date(), forKey: "now")
+print(userDefaults.date(forKey: "now") ?? "?")
