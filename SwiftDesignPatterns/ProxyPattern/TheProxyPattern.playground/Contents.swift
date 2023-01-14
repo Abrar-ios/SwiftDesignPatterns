8381353e17ceb0d2a6aededb83230fb3894a457d
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-15 01:36:05 +0500

    Implement a class with two properties
    * properties are initialized using closure

--- SwiftDesignPatterns/ProxyPattern/TheProxyPattern.playground/Contents.swift
+++ SwiftDesignPatterns/ProxyPattern/TheProxyPattern.playground/Contents.swift
@@ -1,1 +1,13 @@
+import Foundation
 
+class RandomIntWithId {
+    var value: Int = {
+        print("value initialized")
+        return Int.random(in: Int.min...Int.max)
+    }()
+    
+    lazy var uid: String = {
+        print("uid initialized")
+        return UUID().uuidString
+    }()
+}
