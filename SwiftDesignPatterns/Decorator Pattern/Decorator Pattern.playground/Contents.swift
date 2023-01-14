1ccc03a20daefd57e8dc21f883887234be7f7579
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-14 20:46:32 +0500

    Implement Decorator using object wrapper

--- SwiftDesignPatterns/Decorator Pattern/Decorator Pattern.playground/Contents.swift
+++ SwiftDesignPatterns/Decorator Pattern/Decorator Pattern.playground/Contents.swift
@@ -1,1 +1,24 @@
+import Foundation
 
+class UserDefaultsDecorator : UserDefaults {
+    private var userDefaults = UserDefaults.standard
+    
+    convenience init(userDefaults: UserDefaults){
+        self.init()
+        self.userDefaults = userDefaults
+    }
+    
+    func set(date: Date?, forKey key: String) {
+        userDefaults .set(date, forKey: key)
+    }
+    
+    func date(forKey key: String) -> Date? {
+        return userDefaults.value(forKey: key) as? Date
+    }
+}
+
+let userDefaults = UserDefaultsDecorator()
+userDefaults.set(43, forKey: "the answer")
+print(userDefaults.value(forKey: "the answer") ?? "")
+
+userDefaults.set(date: Date(), forKey: "now")
