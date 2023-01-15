ef22cfd93436d8ed67c89b3e2a2b895687621af6
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-15 19:13:25 +0500

    dedicated states implements protocol method
    * each dedicated state implement protocol method to confirm the state
    * if state is failed return the message and turned to standby state in given coffee machine example
    * If state is verified and moved to next state to verify that state

--- SwiftDesignPatterns/TheStateDesignPattern/TheStateDesignPattern.playground/Contents.swift
+++ SwiftDesignPatterns/TheStateDesignPattern/TheStateDesignPattern.playground/Contents.swift
@@ -23,28 +23,66 @@ fileprivate struct StandbyState: CoffeeMachineState {
 
 fileprivate struct FillWaterTankState: CoffeeMachineState {
     var context: CoffeeMachine
-    
+    func isReadyToBrew() -> Bool {
+        guard context.isWaterTankFilled else {
+            print("Fill water tank!")
+            context.state = StandbyState()
+            return false
+        }
+        context.state = EmptyCapsuleBinState(context: context)
+        return context.state.isReadyToBrew()
+    }
 }
 
 fileprivate struct EmptyCapsuleBinState: CoffeeMachineState {
     var context: CoffeeMachine
+    func isReadyToBrew() -> Bool {
+        guard context.isCapsuleBinEmpty else {
+            print("Empty capsule bin tank!")
+            context.state = StandbyState()
+            return false
+        }
+        
+        context.state = InsertCapsuleState(context: context)
+        return context.state.isReadyToBrew()
+    }
     
 }
 
 fileprivate struct InsertCapsuleState: CoffeeMachineState {
     var context: CoffeeMachine
+    func isReadyToBrew() -> Bool {
+        guard context.isCapsuleInserted else {
+            print("Coffee capsule not inserted!")
+            context.state = StandbyState()
+            return false
+        }
+        
+        return true
+    }
     
 }
 
 fileprivate struct BrewCoffeeState: CoffeeMachineState {
     var context: CoffeeMachine
+    func brew() {
+        context.state = FillWaterTankState(context: context)
+        guard context.state.isReadyToBrew() else {
+            print("Something went wrong!")
+            context.state = StandbyState()
+            return
+        }
+        
+        print("Coffee ready")
+        context.state = StandbyState()
+    }
     
 }
 
 class CoffeeMachine {
-    private var isWaterTankFilled: Bool
-    private var isCapsuleBinEmpty: Bool
-    private var isCapsuleInserted: Bool
+    fileprivate var isWaterTankFilled: Bool
+    fileprivate var isCapsuleBinEmpty: Bool
+    fileprivate var isCapsuleInserted: Bool
     
     fileprivate var state: CoffeeMachineState = StandbyState()
 
