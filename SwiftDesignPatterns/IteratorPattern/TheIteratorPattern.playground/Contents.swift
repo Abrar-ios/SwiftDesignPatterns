65e31f87487479f19d65b5f8a67b1693272a297a
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-01-15 17:39:26 +0500

    Implement Iterator using Queue
    * Add Queue extension conforming Sequence protocol
    * Add a structure conforming Iterator protocol
    * This allows to use iterator on custom Queue objects

--- SwiftDesignPatterns/IteratorPattern/TheIteratorPattern.playground/Contents.swift
+++ SwiftDesignPatterns/IteratorPattern/TheIteratorPattern.playground/Contents.swift
@@ -49,8 +49,42 @@ final class Queue<T> {
     }
 }
 
+struct QueueIterator<T>: IteratorProtocol {
+    private let queue: Queue<T>?
+    private var currentNode: Node<T>?
+    
+    init(_ queue: Queue<T>){
+        self.queue = queue
+        self.currentNode = queue.head
+    }
+    
+    mutating func next() -> T? {
+        guard let node = currentNode else {
+            return nil
+        }
+        
+        let nextKey = currentNode?.key
+        currentNode = node.next
+        return nextKey
+    }
+}
+
+extension Queue: Sequence {
+    func makeIterator() -> QueueIterator<T> {
+        QueueIterator(self)
+    }
+}
+
 var queue = Queue<Int>()
 queue.enqueue(1)
 queue.enqueue(2)
 
+for item in queue {
+    print(item)
+}
 
+var queueIterator = queue.makeIterator()
+
+while let item = queueIterator.next() {
+    print(item)
+}
